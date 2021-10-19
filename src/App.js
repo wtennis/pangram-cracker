12976a67c0c7cdfd123badf298fc61e072a17a59
Whiting <whitinghtennis@gmail.com>
2021-10-19 15:41:43 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './puzzles'
 
 function App() {
   return (
@@ -8,26 +9,25 @@ function App() {
         <h4>Instructions</h4>
           <p>Valid answers must be five letters or longer and use the center letter. Pangrams are worth 3 points. All other valid answers are worth 1 point.</p>
       </div>
-      <div id ="puzzle">
-     <h2 id ="letter1">1</h2>
-     <div id = "line2">
-       <h2 id ="letter2">2</h2>
-       <h2 id ="letter3">3</h2>
+      <div id = "answerBar">
+        <h2>Answer Bar</h2> 
+        <button className ="button">Delete</button>
+        <button className ="button">Jumble</button>
+        <button className ="button">Enter</button>
       </div>
-     <h2 id ="letterC">C</h2>
+      <div id ="puzzle">
+     <h2 className = "puzzleLetter">1</h2>
+     <div id = "line2">
+       <h2 className = "puzzleLetter">2</h2>
+       <h2 className = "puzzleLetter">3</h2>
+      </div>
+     <h2 className = "puzzleLetter" id = "letterC">C</h2>
      <div id = "line4">
-      <h2 id ="letter5">5</h2>
-      <h2 id ="letter6">6</h2>
+      <h2 className = "puzzleLetter">5</h2>
+      <h2 className = "puzzleLetter">6</h2>
      </div>
-     <h2 id ="letter7">7</h2>
+     <h2 className = "puzzleLetter">7</h2>
       </div> 
-      
-      <form id="submitter" >
-        <input type="text" id="guess" name="guess" />
-        <button id = "submitButton" type="submit">Submit</button>
-        <button id = "newPuzzle" type="button">New Puzzle</button>
-      </form>
-
 
     </div>
   );
