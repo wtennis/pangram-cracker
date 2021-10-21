0669cff389cca0c3e3c08cab2da730975f8ef46b
Whiting <whitinghtennis@gmail.com>
2021-10-21 12:21:17 -0400

    handleKeydown events

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         id: 5,
         name: 'tcroiea',
         pangrams: ['erotica'],
-        valids: ['coater', 'aortic', 'erotic', 'recoat', 'carte', 'actor', 'citer', 'crate', 'ratio', 'trice', 'cater', 'react', 'trace', 'irate', 'orate', 'errata', 'create', 'recreate']
+        valids: ['coater', 'aortic', 'coterie', 'erotic', 'recoat', 'carte', 'actor', 'citer', 'crate', 'ratio', 'trice', 'cater', 'react', 'trace', 'irate', 'orate', 'errata', 'create', 'recreate']
 
     }
 ]
@@ -39,11 +39,15 @@ function App() {
   const [score, setScore] = useState('')
   const [answerList, setAnswerList] = useState([])
   const [puzzle, setPuzzle] = useState(puzzles[Math.floor(Math.random() * puzzles.length)])
+  const [keyState, setKeyState] = useState('')
+  const [pangram, setPangram] = useState(false)
+  const [valid, setValid] = useState(false)
+
 
   console.log(puzzle)
 
-function handleClick(e){
-  setSubmission(submission + e.target.textContent)
+function handleClick(letter){
+  setSubmission(submission + letter)
 }
 
 function handleDelete(){
@@ -52,71 +56,111 @@ function handleDelete(){
 
 function handleEnter(){
   console.log(submission);
-  let pangram = false
-  let valid = false
-  if (submission.length>6) {pangram = checkPangram()}
-  if (pangram){return}
-  valid = checkValid()
-  console.log(valid)
-  if (valid){return}
-  alert('Not a valid word')
+  if (submission.length<5){
+    alert('submission too short')
+  }else if (submission.length>6) {
+    checkPangram()
+  }else if (submission.length>4 && !pangram) {
+    checkValid()
+  }else if (!pangram && !valid){alert('Not a valid word')}
   setSubmission('')
+  setPangram(false);
+  setValid(false);
 }
 
 function checkPangram(){
   puzzle.pangrams.forEach((pangram) => { 
     if (submission.toLowerCase() === pangram){
+        setPangram(true)
         setScore(score + 3);
         setSubmission('');
         alert('pangram!')
-        return true;
     }
   })
-  return false
 }
 
 function checkValid(){
   puzzle.valids.forEach((valid) => { 
     if (submission.toLowerCase() === valid){
+        setValid(true)
         setScore(score + 1);
         setSubmission('');
         alert('valid!')
-        return true;
     }
   })
+}
+
+document.addEventListener('keydown', handleKeyDown)
+
+function handleKeyDown(e){
+  switch (e.key) {
+    case 'Enter':
+      console.log('Enter pressed');
+      handleEnter();
+      break;
+    case puzzle.name[0]:
+    handleClick(puzzle.name[0])
+      break;
+    // case puzzle.name[1]:
+    //   handleClick(puzzle.name[1])
+    //     break;
+    // case puzzle.name[2]:
+    //   handleClick(puzzle.name[2])
+    //     break;
+    // case puzzle.name[3]:
+    //   handleClick(puzzle.name[3])
+    //     break;
+    //     case puzzle.name[4]:
+    //       handleClick(puzzle.name[4])
+    //         break;
+    //   case puzzle.name[5]:
+    //     handleClick(puzzle.name[5])
+    //       break;
+    //     case puzzle.name[6]:
+    //       handleClick(puzzle.name[6])
+    //         break;
+        case "Delete":
+          handleDelete();
+            break;
+    default:
+      console.log(`What key did you press?`);
+  }
+  console.log(e.key, 'pressed')
 }
 
   return (
     <div id = "container">
     <h1>Pangram Cracker</h1>
+    <h1>{keyState}</h1>
       <div className = "instructions">
-        <h4>Instructions</h4>
+        <h4 id= "instructions-header">Instructions</h4>
           <p>Valid answers must be five letters or longer and use the center letter. Pangrams are worth 3 points. All other valid answers are worth 1 point.</p>
       </div>
+      <h3 id = "score">Score: {score}</h3>
       <div id = "answerBar">
         <h2 id = "submission">{submission}</h2> 
-        <button onClick={handleDelete} className ="button">Delete</button>
-        <button className="button">Jumble</button>
-        <button className="button" onClick={handleEnter}>Enter</button>
+        <button onClick={handleDelete} className ="button" style={{cursor: "pointer"}}>Delete</button>
+        <button className="button" style={{cursor: "pointer"}}>Jumble</button>
+        <button className="button" onClick={handleEnter} style={{cursor: "pointer"}}>Enter</button>
       </div>
       <div id ="puzzle">
      <h2 
+     style={{cursor: "pointer"}}
      className="puzzleLetter" 
-     onClick={(e) => handleClick(e)}
+     onClick={(e) => handleClick(e.target.textContent)}
      >{puzzle.name[1]}
      </h2>
      <div id = "line2">
-       <h2 className = "puzzleLetter" onClick={(e) => handleClick(e)}>{puzzle.name[2]}</h2>
-       <h2 className = "puzzleLetter" onClick={(e) => handleClick(e)}>{puzzle.name[3]}</h2>
+       <h2 style={{cursor: "pointer"}}className = "puzzleLetter" onClick={(e) => handleClick(e.target.textContent)}>{puzzle.name[2]}</h2>
+       <h2 style={{cursor: "pointer"}}className = "puzzleLetter" onClick={(e) => handleClick(e.target.textContent)}>{puzzle.name[3]}</h2>
       </div>
-     <h2 className="puzzleLetter" id="letterC" onClick={(e) => handleClick(e)}>{puzzle.name[0]}</h2>
+     <h2 style={{cursor: "pointer"}}className="puzzleLetter" id="letterC" onClick={(e) => handleClick(e.target.textContent)}>{puzzle.name[0]}</h2>
      <div id = "line4">
-      <h2 className="puzzleLetter" onClick={(e) => handleClick(e)}>{puzzle.name[4]}</h2>
-      <h2 className = "puzzleLetter" onClick={(e) => handleClick(e)}>{puzzle.name[5]}</h2>
+      <h2 style={{cursor: "pointer"}}className="puzzleLetter" onClick={(e) => handleClick(e.target.textContent)}>{puzzle.name[4]}</h2>
+      <h2 style={{cursor: "pointer"}}className = "puzzleLetter" onClick={(e) => handleClick(e.target.textContent)}>{puzzle.name[5]}</h2>
      </div>
-     <h2 className = "puzzleLetter" onClick={(e) => handleClick(e)}>{puzzle.name[6]}</h2>
+     <h2 style={{cursor: "pointer"}}className = "puzzleLetter" onClick={(e) => handleClick(e.target.textContent)}>{puzzle.name[6]}</h2>
       </div> 
-      <h3 id = "score">Score: {score}</h3>
     </div>
   );
 }
